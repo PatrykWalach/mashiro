16914ca3dc22ebf76b82d00ea8a80dd46e8e37a8
PatrykWalach <patryk_walach@outlook.com>
2020-12-07 13:17:38 +0100

    feat: add shims-voldar.d.ts

---
+++ src/shims-volar.d.ts
@@ -0,0 +1,25 @@
+import {
+  RouterLink,
+  RouterView,
+  useLink,
+  RouteLocationNormalized,
+} from 'vue-router'
+import { UnwrapRef, VNode } from 'vue'
+
+declare global {
+  interface __VLS_GlobalComponents {
+    RouterLink: typeof RouterLink & {
+      __VLS_slots: {
+        default: UnwrapRef<ReturnType<typeof useLink>>
+      }
+    }
+    RouterView: typeof RouterView & {
+      __VLS_slots: {
+        default: {
+          Component: VNode
+          route: RouteLocationNormalized & { href: string }
+        }
+      }
+    }
+  }
+}
