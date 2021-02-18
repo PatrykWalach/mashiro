0da9eeda4a69e65000003a35f3dc7b0fb98051c1
PatrykWalach <patryk_walach@outlook.com>
2021-02-18 22:53:00 +0100

    feat: add windicss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-var-requires*/
+const colors = require('windicss/colors')
+
+const typography = require('windicss/plugin/typography')
+
+module.exports = {
+  darkMode: 'class',
+  plugins: [typography],
+  theme: {
+    extend: {
+      colors: {
+        teal: colors.teal,
+      },
+    },
+  },
+}
--- vue.config.js
+++ vue.config.js
@@ -2,16 +2,33 @@
 const CircularDependencyPlugin = require('circular-dependency-plugin')
 
 module.exports = {
-  configureWebpack: {
-    plugins: [
+  configureWebpack: config => {
+    config.module.rules.push({
+      test: /\.vue$/,
+      use: [
+        {
+          loader: 'vue-windicss-preprocess',
+          options: {
+            config: 'tailwind.config.js', // tailwind config file path OR config object (optional)
+            compile: false, // false: interpretation mode; true: compilation mode
+            globalPreflight: true, // set preflight style is global or scoped
+            globalUtility: true, // set utility style is global or scoped
+            prefix: 'windi-', // set compilation mode style prefix
+          },
+        },
+      ],
+    })
+
+    config.plugins.push(
       new CircularDependencyPlugin({
         exclude: /node_modules/,
         failOnError: true,
         allowAsyncCycles: false,
         cwd: process.cwd(),
       }),
-    ],
+    )
   },
+
   pluginOptions: {
     electronBuilder: {
       // mainProcessTypeChecking: true,
