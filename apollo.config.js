93b2a3630b45b7325cf55484607119415b2fcbf3
Patryk Walach <patryk_walach@outlook.com>
2021-02-28 17:21:09 +0100

    fix: localSchemaFile path

--- apollo.config.js
+++ apollo.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   client: {
-    service: { localSchemaFile: './schema.graphql' },
+    service: { localSchemaFile: 'schemas/nexus.graphql' },
     includes: ['src/**/*.vue'],
   },
 }
